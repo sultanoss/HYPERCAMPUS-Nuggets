d552d135fc0b3ed51177ec2b74fe99948eaabba9
sultanoss <radwansultan@hotmail.de>
2022-08-08 17:11:51 +0200

    responsive designe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'HYPERCAMPUS-Nuggets';
   searchText = '';
+  mobileMenu: boolean = false;
+  openMobileMenu(){
+    this.mobileMenu = true;
+  }
+  closeMobileMenu(){
+    this.mobileMenu = false;
+  }
 }
